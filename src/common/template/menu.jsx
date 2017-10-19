210478f3f236fa31dc11c30132f807b5eb8073d3
Igor Queiroz <igorcenourox@gmail.com>
2017-10-19 17:48:26 -0300

    Primeiro commit do front-end

---
+++ src/common/template/menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import MenuItem from './MenuItem'
+import MenuTree from './menuTree'
+
+export default props => (
+  <ul className="sidebar-menu">
+    <MenuItem path='#' label='DashBoard' icon='dashboard' />
+    <MenuTree label="Register" icon="edit">
+      <MenuItem path="#billingCycles" label="Billing Cycles" icon="usd" />
+    </MenuTree>
+  </ul>
+)
---
+++ src/main/routes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Router, Route, Redirect, hashHistory } from 'react-router'
+
+import DashBoard from '../dashboard/dashboard'
+import BillingCycle from '../billingCycle/billingCycle'
+
+export default props => (
+  <Router history={hashHistory}>
+    <Route path='/' component={DashBoard} />
+    <Route path='/billingCycles' component={BillingCycle} />
+    <Redirect from='*' to="/" />
+  </Router>
+)
